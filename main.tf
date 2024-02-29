91e87004afe506e13ff2ca45d0d8affe2ecabd18
ariel.molina <ariel.molina@caosbinario.com>
2024-02-29 20:58:55 -0300

    variables

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+####### Variables #######
+
+variable "ami_id" {
+  description = "ID de la AMI para la instancia EC2"
+  default     = "ami-0440d3b780d96b29d"
+}
+
+variable "instance_type" {
+  description = "Tipo de instancia EC2"
+  default     = "t3.micro"
+}
+
+variable "server_name" {
+  description = "Nombre del servidor web"
+  default     = "nginx-server"
+}
+
+variable "environment" {
+  description = "Ambiente de la aplicación"
+  default     = "test"
+}
+
 ####### provider #######
 provider "aws" {
   region = "us-east-1"
@@ -5,8 +27,8 @@ provider "aws" {
 
 #######  resource ####### 
 resource "aws_instance" "nginx-server" {
-  ami           = "ami-0440d3b780d96b29d"
-  instance_type = "t3.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +44,8 @@ resource "aws_instance" "nginx-server" {
   ]
 
   tags = {
-    Name        = "nginx-server"
-    Environment = "test"
+    Name        = var.server_name
+    Environment = var.environment
     Owner       = "ariel.molina@caosbinario.com"
     Team        = "DevOps"
     Project     = "webinar"
@@ -34,12 +56,12 @@ resource "aws_instance" "nginx-server" {
 # ssh-keygen -t rsa -b 2048 -f "nginx-server.key"
 
 resource "aws_key_pair" "nginx-server-ssh" {
-  key_name   = "nginx-server-ssh"
-  public_key = file("nginx-server.key.pub")
+  key_name   = "${var.server_name}-ssh"
+  public_key = file("${var.server_name}.key.pub")
 
   tags = {
-    Name        = "nginx-server-ssh"
-    Environment = "test"
+    Name        = "${var.server_name}-ssh"
+    Environment = "${var.environment}"
     Owner       = "ariel.molina@caosbinario.com"
     Team        = "DevOps"
     Project     = "webinar"
@@ -48,7 +70,7 @@ resource "aws_key_pair" "nginx-server-ssh" {
 
 ####### SG ####### 
 resource "aws_security_group" "nginx-server-sg" {
-  name        = "nginx-server-sg"
+  name        = "${var.server_name}-sg"
   description = "Security group allowing SSH and HTTP access"
 
   ingress {
@@ -73,8 +95,8 @@ resource "aws_security_group" "nginx-server-sg" {
   }
 
   tags = {
-    Name        = "nginx-server-sg"
-    Environment = "test"
+    Name        = "${var.server_name}-sg"
+    Environment = "${var.environment}"
     Owner       = "ariel.molina@caosbinario.com"
     Team        = "DevOps"
     Project     = "webinar"
