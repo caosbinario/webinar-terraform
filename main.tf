c4953e37821743ff34900cf86b059dceda944bd7
ariel.molina <ariel.molina@caosbinario.com>
2024-02-29 20:28:24 -0300

    nginx + ssh key + sg

--- main.tf
+++ main.tf
@@ -7,4 +7,51 @@ provider "aws" {
 resource "aws_instance" "nginx-server" {
   ami           = "ami-0440d3b780d96b29d"
   instance_type = "t3.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y nginx
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
+
+  key_name = aws_key_pair.nginx-server-ssh.key_name
+  
+  vpc_security_group_ids = [
+	aws_security_group.nginx-server-sg.id
+  ]
+
+}
+
+####### ssh ####### 
+resource "aws_key_pair" "nginx-server-ssh" {
+  key_name   = "nginx-server-ssh"
+  public_key = file("nginx-server.key.pub")
+}
+
+####### SG ####### 
+resource "aws_security_group" "nginx-server-sg" {
+  name        = "nginx-server-sg"
+  description = "Security group allowing SSH and HTTP access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 }
