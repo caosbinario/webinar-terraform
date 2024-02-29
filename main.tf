d2e87680fbcfc058f919e3b4111a5243c157f161
ariel.molina <ariel.molina@caosbinario.com>
2024-02-29 20:48:26 -0300

    outputs

--- main.tf
+++ main.tf
@@ -80,3 +80,14 @@ resource "aws_security_group" "nginx-server-sg" {
     Project     = "webinar"
   }
 }
+
+#######  output ####### 
+output "server_public_ip" {
+  description = "Dirección IP pública de la instancia EC2"
+  value       = aws_instance.nginx-server.public_ip
+}
+
+output "server_public_dns" {
+  description = "DNS público de la instancia EC2"
+  value       = aws_instance.nginx-server.public_dns
+}
