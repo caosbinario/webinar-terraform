ed7e3ec6dd31a5a4efc7b46af691750f6a8d73d1
ariel.molina <ariel.molina@caosbinario.com>
2024-02-29 20:29:00 -0300

    ssh key command

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ resource "aws_instance" "nginx-server" {
 }
 
 ####### ssh ####### 
+# ssh-keygen -t rsa -b 2048 -f "nginx-server.key"
+
 resource "aws_key_pair" "nginx-server-ssh" {
   key_name   = "nginx-server-ssh"
   public_key = file("nginx-server.key.pub")
